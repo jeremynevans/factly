eef968c112a794bab8062114a21cb3ea2cbe2c0e
Jeremy Evans <jeremy.n.evans@gmail.com>
2018-01-18 19:46:50 +0100

    More Norway facts!

--- app/facts/norway.js
+++ app/facts/norway.js
@@ -18,4 +18,54 @@ exports.facts = [
   '"God kveld" - Good evening. ',
 
   '"God natt" - Good night.',
+
+  '1. The original city was founded around 1050 by King Harald Hardrada, but destroyed in a fire in 1624. So Christian IV of Denmark-Norway set up a new town further west, and changed the name to Christiania. The spelling was switched to Kristiania in 1877, but Parliament reverted to Oslo in 1925.',
+
+  '2. In 1814, Norway’s first constitution designated Christiania as the official capital.',
+
+  '3. Oslo is Norway’s most populous city with 647,676 residents as of January 2015, comprising 12.5 percent of the country’s total population.',
+
+  '4. If Oslo\'s average temperature of 22°F in January feels a little chilly, you should just go ahead and blame yourself: “There’s no bad weather, just bad clothing choices,” as a popular Norwegian saying goes.',
+
+  '5. Norway’s King and Queen live at the Royal Palace, located at the top of Karl Johans Gate (essentially Oslo\'s Main Street). Construction began in 1824; the palace was first used in 1849 by King Oscar I. Foreign heads of state also stay here when visiting the capital on business, and the public can tour the royal residence during the summer (ticket sales for this season started earlier this month).',
+
+  '6. There are actually four existing versions of Edvard Munch’s most famous work, The Scream. An 1893 tempera-and-crayon one hangs in Oslo’s National Museum of Art, Architecture and Design, or Nasjonalmuseet, while a 1893 pastel and a 1910 painting are at the city’s Munch Museum, or Munchmuseet. The fourth, produced in 1895, which bought for around $120 million at a Sotheby’s auction.',
+
+  '7. The Scream depicts the vantage point from the hilltop of Oslo’s Ekeberg neighborhood, with the fjord, town, and hills below.',
+
+  '8. Get lost in the 62-acre sculpture park Ekebergparken, which was opened in 2013 by philanthropist and collector Christian Ringnes. As of today, 34 sculptures are peppered throughout the grounds, including works by Salvador Dali, Auguste Rodin, and Pierre-Auguste Renoir. A highlight? James Turrell’s Skyspace: The Color Beneath, which changes hues, thus altering the viewer\'s perception of the work—and his or her perception of "the sky above," as the work\'s description reads.',
+
+  '9. A naked man juggling four infants, an angry stomping baby, and a 46-foot high tower of 121 humans are just a few of the more than 200 art pieces at Vigeland Sculpture Park located inside Oslo’s biggest greenspace, Frogner Park. Sculptor Gustav Vigeland made every one of the sculptures himself and also designed the architecture of the 80-acre park, which was finished between 1939 and 1949.',
+
+  '10. The sloped marble top of the Oslo Opera House, designed by architecture firm Snøhetta, resembles an iceberg jetting out of a fjord—and visitors are encouraged to climb the roof all year long. The 414,411-square-foot building, which opened in 2008 and cost $665 million to build, sits on the Oslo waterfront, and is home to the Norwegian National Opera and Ballet.',
+
+  '11. Just a 15-minute metro ride inland from the capital’s center is a forest, Oslomarka, completely within the city limits of Oslo.',
+
+  '12. As a kid, James and the Giant Peach and Charlie and the Chocolate Factory author Roald Dahl used to spend his summer vacations visiting his grandparents in Oslo.',
+
+  '13. Every year since 1947, Oslo has given London its Trafalgar Square Christmas tree as a thank you gift for the UK\'s support during World War II. The city has also given Reykjavik one annually since 1951. But in 2014, Fabian Stang, who was then the mayor of Oslo, said they would no longer be gifting a tree to the Icelandic capital because of the high costs involved. After then-mayor of Reykjavik Jon Gnarr fired back, the tradition was reinstated without a break.',
+
+  '14. The city’s Viking Ship Museum houses three historical boats: the Oseberg, made of oak in the year 820; the Tune, which was the first Viking ship to be excavated when it was found in 1867; and the world\'s best-preserved Viking ship, the Gokstad, discovered in a burial mound on a farm in 1879.',
+
+  '15. A noble date: Every year on December 10, the Nobel Peace Prize is awarded in the presence of Norway’s King Harald V at Oslo City Hall. The other Nobel Prizes are presented in Stockholm.',
+
+  '16. The Akershus Castle and Fortress, which was used during the Middle Ages, still serves as a military base. Look familiar? There\'s a replica of it at Walt Disney World’s Epcot in Lake Buena Vista, Florida.',
+
+  '17. Norwegian playwright Henrik Ibsen worked in theaters in both Bergen and Oslo, but moved to Germany where he wrote his most famous work, A Doll\'s House. Later in life, he returned to Oslo, where he died on May 23, 1906.',
+
+  '18. In February, Oslo’s Maaemo restaurant was awarded three Michelin stars, making it the only restaurant in the country with the honor. The inspectors said the cooking at the restaurant, whose name means "Mother Earth," is "intricate, original and visually stimulating with some sublime flavor combinations.”',
+
+  '19. The Oslo Airport is the first in the world to offer biofuel to all airlines, and Lufthansa was the first to use the Air BP aviation biofuel in an Airbus A320 on January 22.',
+
+  '20. Music lovers visiting the Norwegian city have a wide variety of events to choose from. There\'s the Inferno Metal Festival in March, and the Norwegian Wood Rock Festival in June. Oh, and then the Oslo Jazz Festival in August, the Oslo World Music Festival in November, and the Oslo International Church Music Festival in March.',
+
+  '21. Who said bigger is better? The city’s Mini Bottle Gallery is the only museum of its kind on the planet. It contains 53,000 bottles, 12,500 of which are on display across the 16,147-square-foot space.',
+
+  '22. The Holmenkollen ski jump hosted competitions annually from 1892 to 2008, and was even the site of the World Championships in 1982. A new jump was built in 2010, and Holmenkollen is now known as the world’s most modern ski jump, featuring 1000 tons of steel over the 440-foot long hill. The observation deck provides panoramic views, and more daring visitors can zipline down the hill for about $70.',
+
+  '23. The big cheese in town: Brunost, which translates to "brown cheese." But the tan-colored slices aren’t actually cheese at all. The fudge-like texture and sweet-yet-salty taste comes from the caramelization of goat’s milk whey.',
+
+  '24. Oslo was ranked the most expensive city in the world in 2013, but this year it dropped out of the top 10, coming in at number 13.',
+
+  '25. Play more than 100 video games from 1972 to today at the Game On 2.0 exhibit at the Norwegian Museum of Science & Technology, or Teknisk Museum (through January 29, 2017). Visitors can challenge each other to the original arcade versions of Pac-Man, Donkey Kong, Asteroids, and more.'
 ]
