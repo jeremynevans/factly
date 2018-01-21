0774adc25e23df7466c572dee541da82b543c9fb
Jeremy Evans <jeremy.n.evans@gmail.com>
2018-01-21 15:09:00 +0100

    czech new

--- app/facts/czech.js
+++ app/facts/czech.js
@@ -1,21 +1,84 @@
 exports.facts = [
-  '1. According to the Guinness Book of World Records, the largest castle in the world is in Prague. With the incredible area of 18 acres, ladies and gentlemen, this is the Prague Castle!',
+  // '1. According to the Guinness Book of World Records, the largest castle in the world is in Prague. With the incredible area of 18 acres, ladies and gentlemen, this is the Prague Castle!',
+  //
+  // '2. After the murder of John Lennon in 1980, a portrait of the rock star was drawn on the wall opposite the French Embassy in Prague. Although the wall was repainted numerous times after that, until today it’s been known as the John Lennon Wall, always covered in quotes, images, and lyrics from his songs. It is located in Mala Strana. Read the full story of the John Lennon Wall in Prague. [Fun fact: John Lennon never visited Prague.]',
+  //
+  // '3. Every hour the Prague Astronomical Clock strikes but not in a way you would expect. A whole procession sets off including the Twelve Apostles and Death and finishing off with a golden rooster crowing. The clock can also give you accurate information about the day of the year, the zodiac signs and the Medieval perception of the Universe with the Earth in its center.',
+  //
+  // '4. Speaking about the Astronomical Clock, it’s good to know that Prague is not the only place you can find it. There’s a replica of the building thousands of miles to the East – in Seoul, South Korea. The Korean twin of the Clock serves as a restaurant and expo center.',
+  //
+  // '5. The Jewish Quarter of Prague was not demolished in World War II. The legend goes that the reason was that Hitler planned to retire in Prague. He wanted to turn the Josehof Quarter in a museum, dedicated to an extinct race.',
+  //
+  // '6. Charle’s Bridge was inaugurated at 5:31 in the morning on July 9, 1357. The reason behind this peculiar decision is that the king at the time was a maniac when it came to astrology and numerology. He believed it would be a good fortune if he arranged the numbers symmetrically: 1-3-5-7-9-7-5-3-1.',
+  //
+  // '7. The narrowest street in Prague is barely 50 cm (20 inches) wide, yet it has deserved the right to have its own traffic light. The traffic light prevents pedestrians from meeting in the middle and then wondering how to walk past each other. You can find it close to Charle’s Bridge. Read more about the narrowest street in Prague.',
+  //
+  // '8. Both loved and hated, the TV tower is probably most famous for its black crawling babies – sculptures by David Černý. Very few people are aware, however, that there’s a one-room hotel in the tower with an amazing view over Prague from 70 m.',
+  //
+  // '9. Rumor has it that after one of their first major concerts in post-communist Prague the members of Rolling Stones liked the Prague Castle so much that they wanted to pay to make it more beautiful at night. They shared their idea with the former Czech president Václav Havel, who explained to them that the country had more serious problems to cope with. The band members offered to pay for art night lighting for the castle. So, $32,000 later, their stage light technician created the lighting of the castle, still in use today.',
+  //
+  // '10. Prague was the first post-communist country to have a Michelin-star restaurant. ',
 
-  '2. After the murder of John Lennon in 1980, a portrait of the rock star was drawn on the wall opposite the French Embassy in Prague. Although the wall was repainted numerous times after that, until today it’s been known as the John Lennon Wall, always covered in quotes, images, and lyrics from his songs. It is located in Mala Strana. Read the full story of the John Lennon Wall in Prague. [Fun fact: John Lennon never visited Prague.]',
+  // '#1. Due to its lack of international and domestic conflict as well as its low crime rate, the Global Peace Index in 2016 ranks the Czech Republic as the sixth safest place to live on earth.',
+  //
+  // '#2. The language has a formal and an informal form, and the natives are patient with foreigners who use them inappropriately. They are proud of their Czech language, which the U. S. Foreign Institute ranks as the second most difficult language to learn. They revel in their ability to master it.',
+  //
+  // '#3. Puppetry and marionette exhibitions are very popular for both children and adults and puppetry festivals are held throughout the country each year. The puppet museum in Prague hosts shows daily and has the largest collection in the world.',
+  //
+  // '#4. The Czech Republic is known worldwide for its mouth blown decorated art glass and crystal, all individually made.',
+  //
+  // '#5. The Czech people have the highest beer consumption per capita in the world. Its first known brewery existed in 993. Pilsners were originated here as did the first ever blond pilsners. There are several beer festivals hosted annually.',
+  //
+  // '#6. The Jewish population of Bohemia and Moravia was virtually annihilated during the Holocaust by the Nazis. In 1930 there were 118,000 Jews according to the census and the 2005 census lists only 4000.',
+  //
+  // '#7. The Czech Republic ranks as one of the least religious populations in the world, with only 19 percent of them claiming to believe in God.',
+  //
+  // '#8. Mushroom hunting is a favorite pastime of the Czech people. In the autumn it is a national passion in the damp forests. They gather yearly at St. Václav Day in September and the hunts can be quite competitive.',
+  //
+  // '#9. The Czech Republic is a true paradise for people who love parks and gardens. It has 25 protected landscaped areas and four national parks in addition to broadleaf and conifer forests, rivers and mountains.',
+  //
+  // '#10. Tourism is a big business in the Czech Republic. It has an impressive architectural heritage with 12 UNESCO monuments and many centuries’ worth of monuments, buildings, towers, bridges, and castles to visit and admire.',
+  //
+  // '#11. Prague has been known as the “handsomest city of Europe” since the 18th century. Prague Castle is the largest in Europe with more than 700 hundred rooms. The pedestrian Charles Bridge over the Vitava River fills with painters, vendors, musicians and tourists every summer.',
+  //
+  // '#12. Known as the castle capital of the world, the republic has over 2000 castles, ruins and keeps for visitors to tour and enjoy. The St. Vitrus Cathedral at Prague Castle is home of the Czech crown jewels and the burial place of Czech kings.',
+  //
+  // '#13. The Old Town Square is home of the Astronomical Clock, the annual Christmas market, cafes, street entertainers, the Old Town Hall from 1338 and baroque era buildings (See below.)',
+  //
+  // '#14. The spa towns of Karlovy Vary, Mariańské Láznĕ, Františkovy Láznĕ, and Jáchymov offer oases of comfort and relaxation for those who want a stress-free holiday.',
+  //
+  // '#15. For the youngsters, the Czech Aquapalace Praha is the biggest water park in Central Europe.',
+  //
+  // '#16. Some Czechs who are international renowned in their fields of endeavor include Gregor Mendel (science of genetics), Alphonse Mucha (visual arts), Franz Kafka (literary arts), plus Martina Navratilova and Ivan Lendl (Sports-tennis).',
+  //
+  // '#17. Inventors have abounded throughout the history of the Czech people, and Czechs have invented the first grounded lightning rod, the screw propeller, the modern compass, sugar cubes, photogravure, the arc lamp, the plastic explosive Semtex, and soft contact lenses, among other things.',
+  //
+  // '#18. Czech citizens Frantĭsek Burian and Arnold Jirásek founded the first plastic surgery in 1927.',
+  //
+  // '#19. The Czech economy gains a substantial income from their tourism industry.',
+  //
+  // '#20. The Czech Republic has an advanced, high income economy and a high standard of living. In fact, it is the most stable and prosperous of the post-communist states, with the lowest unemployment rate in the European Union.',
 
-  '3. Every hour the Prague Astronomical Clock strikes but not in a way you would expect. A whole procession sets off including the Twelve Apostles and Death and finishing off with a golden rooster crowing. The clock can also give you accurate information about the day of the year, the zodiac signs and the Medieval perception of the Universe with the Earth in its center.',
-
-  '4. Speaking about the Astronomical Clock, it’s good to know that Prague is not the only place you can find it. There’s a replica of the building thousands of miles to the East – in Seoul, South Korea. The Korean twin of the Clock serves as a restaurant and expo center.',
-
-  '5. The Jewish Quarter of Prague was not demolished in World War II. The legend goes that the reason was that Hitler planned to retire in Prague. He wanted to turn the Josehof Quarter in a museum, dedicated to an extinct race.',
-
-  '6. Charle’s Bridge was inaugurated at 5:31 in the morning on July 9, 1357. The reason behind this peculiar decision is that the king at the time was a maniac when it came to astrology and numerology. He believed it would be a good fortune if he arranged the numbers symmetrically: 1-3-5-7-9-7-5-3-1.',
-
-  '7. The narrowest street in Prague is barely 50 cm (20 inches) wide, yet it has deserved the right to have its own traffic light. The traffic light prevents pedestrians from meeting in the middle and then wondering how to walk past each other. You can find it close to Charle’s Bridge. Read more about the narrowest street in Prague.',
-
-  '8. Both loved and hated, the TV tower is probably most famous for its black crawling babies – sculptures by David Černý. Very few people are aware, however, that there’s a one-room hotel in the tower with an amazing view over Prague from 70 m.',
-
-  '9. Rumor has it that after one of their first major concerts in post-communist Prague the members of Rolling Stones liked the Prague Castle so much that they wanted to pay to make it more beautiful at night. They shared their idea with the former Czech president Václav Havel, who explained to them that the country had more serious problems to cope with. The band members offered to pay for art night lighting for the castle. So, $32,000 later, their stage light technician created the lighting of the castle, still in use today.',
-
-  '10. Prague was the first post-communist country to have a Michelin-star restaurant. ',
+  'The Czech Republic is almost entirely surrounded by mountains (except to the south, toward Austria and Slovakia). Mountains mark a natural border with Germany and Poland.',
+  '90% of Czechs have completed at least secondary education - the highest score in the EU (along with Poles, Slovaks, and Slovenes).',
+  'According to the 2001 census, 59% of the Czechs are Agnostic, Atheist, or non-believer, 26.8% Roman Catholic and 2.5% Protestant. Only 19% said that they believed in God, meaning that about 11% of the self-declared Christians were only Christian in name (or by tradition).',
+  'The Czech Republic has the most hospital beds per inhabitant in the EU.',
+  'Czech people have the second highest death rate for cancer in the EU (after Hungary).',
+  'Czech people are the world\'s heaviest consumers of beer.',
+  'Czech and Slovak languages are mutually intelligible to people accustomed to the other language\'s pronuciation, particularily people who have lived at the time of Czechoslovakia (the country split in 1993) . The Sorbian languages spoken by a minority of people in south-eastern Germany (Saxony and Brandenburg) are also closely related to Czech language. The relation between these West Slavic languages is a similar to that of Castillan Spanish, Catalan, Galician and Portuguese together.',
+  'Czech people are mostly of Slavic descent, but many people can also claim partial German ancestry, as a result of the country\'s 1000 years within the Holy Roman Empire or Austrian Empire.',
+  'The country is renowned for its numerous spa towns, especially along the border with Saxony (Germany). The most famous ones are those of Karlovy Vary (Karlsbad), Františkovy Lázně (Franzensbad) and Teplice (Teplitz).',
+  'There are over 2,000 castles, keeps, and castle ruins in the Czech Republic, one of the highest density in the world (notably after Belgium and France).',
+  'One of the most famous Czech sportswoman is tennis champion Martina Navratilova (born in 1956), statistically the second best female player of the 20th century behind Steffi Graf.',
+  'Economy',
+  'The Czech Republic is the second richest Eastern European country after tiny Slovenia. The GDP per capita (PPP) of Czech people is is similar to that of Portuguese or Greek people.',
+  'Prague\'s GDP per capita (PPP) is twice the Czech national average, and higher than any EU member states\' national average except Luxembourg. Prague has the 12th highest GDP per capita (PPP) of the 269 NUTS-2 Region (province level) in the European Union, and the highest outside the UK, the Benelux, France, Germany, Austria and Sweden.',
+  'Within the EU, part-timers work the longest hours in the Czech Republic (about 24h/week). However, part-time work is very rare in this country.',
+  'Reporters Without Borders ranked the Czech Republic as the 5th best country in the world (out of 168 countries listed) for freedom of press in 2006.',
+  'History',
+  'Historically, the Czech Republic was composed of the Kingdom of Bohemia and the March of Moravia, both part of the the Holy Roman Empire (800-1806), then also within the Habsburg Empire (1526-1918).',
+  'Bohemia was first a duchy in the 9th century, then was elevated to a kingdom in 1198. Most of the early Kings of Bohemia belonged to the powerful House of Luxembourg (from 1310 to 1437) and combined the title of Holy Roman Emperor. The only daughter of Sigismund of Luxembourg married Albert of Habsburg, and the crown of Bohemia subsequently passed to the House of Habsburg, which kept it until the dissolution of their empire in 1918.',
+  'The Charles University in Prague, founded in 1348, is the oldest university in Eastern Europe, and one of the oldest in the world in continuous operation. According to the 2006 Academic Ranking of World Universities by Shanghai Jiao Tong University, it is the leading university in Eastern Europe.',
+  'Famous Czech people include the religious reformer Jan Hus (1370-1415), the early proponent of universal education John Amos Comenius (1592-1670), the classical music composers Bedrich Smetana (1824-1884) and Antonín Dvorák (1841-1904), and the German-language author Franz Kafka (1883-1924).',
+  'A Czech, the chemist Otto Wichterle, invented the soft contact lenses in 1959.',
 ]
